cb57b44370fa641cb19ea5e098d5219ab1a8f2c4
priyadassjoseph <62575932+priyadassjoseph@users.noreply.github.com>
2020-10-15 15:56:39 -0700

    Add files via upload

---
+++ Presentation/Employee.API/Controllers/EmployeeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+
+namespace Employee.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _service;
+        private readonly IMapper _mapper;
+        public EmployeeController(IEmployeeService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+        #region GET
+        /// <summary>
+        /// Comments and descriptions can be added to every endpoint using XML comments.
+        /// </summary>
+        /// <remarks>
+        /// XML comments included in controllers will be extracted and injected in Swagger/OpenAPI file.
+        /// </remarks>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<Employee> Get(string id)
+        {
+            var data = await _service.GetAsync(id);
+
+            if (data != null)
+                return _mapper.Map<Employee>(data);
+            else
+                return null;
+        }
+        #endregion
+
+        #region POST
+        [HttpPost]
+        public async Task<Employee CreateEmployee([FromBody]EmployeeCreationRequest value) //
+        {
+
+            //TODO: include exception management policy according to technical specifications
+            if (value == null)
+                throw new ArgumentNullException("value");
+
+            if (value == null)
+                throw new ArgumentNullException("value.Employee");
+
+
+            var data = await _service.CreateAsync(Mapper.Map<S.Employee>(value.Employee));
+
+            if (data != null)
+                return _mapper.Map<Employee>(data);
+            else
+                return null;
+
+        }
+        #endregion
+    }
+}
